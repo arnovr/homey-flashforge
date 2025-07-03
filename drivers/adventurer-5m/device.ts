ddb2140c5483d186a97388fe175bbad2dd1b2788
arnovr <arnovanrossum1@gmail.com>
2025-07-03 13:21:14 +0200

    Extract flash forge client on device, i want it to be a facade as the API might change a few times from external perspective

--- drivers/adventurer-5m/device.ts
+++ drivers/adventurer-5m/device.ts
@@ -1,5 +1,5 @@
-import { FlashForgeClient } from 'ff-5mp-api-ts';
 import Homey from 'homey';
+import { FlashForgeClient } from './FlashForgeClient';
 
 module.exports = class FlashForgeDevice extends Homey.Device {
   client: FlashForgeClient | undefined;
@@ -8,11 +8,14 @@ module.exports = class FlashForgeDevice extends Homey.Device {
   async onInit() {
     this.log('Initialize device');
     this.setCapabilityValue('onoff', false);
+
     const ip = this.getData().ip || this.getSetting('ip');
     this.client = new FlashForgeClient(ip);
-    // Initialisation update
-    await this.updateStatus()
+
+    await this.updateStatus();
+
     this.registerCapabilityListener("onoff", this.handleButton.bind(this));
+
     this.pollInterval = this.homey.setInterval(() => {
       this.updateStatus();
     }, 30000);
@@ -23,91 +26,56 @@ module.exports = class FlashForgeDevice extends Homey.Device {
       throw new Error("Er is geen client geïnitialiseerd.");
     }
 
-    await this.client.initControl();
+    const isPrinting = await this.client.isPrinting();
 
-    try {
-      const printStatus = await this.client.getPrintStatus();
-      const printPercent = printStatus?.getPrintPercent();
-
-      const isPrinting = typeof printPercent === 'number' && !isNaN(printPercent);
-
-      if (isPrinting) {
-        if(printPercent != 100) { // It keeps a bit on 100%, Cant pause it on 100%
-          if (value) {
-            await this.client.resumeJob();
-          } else {
-            await this.client.pauseJob();
-          }
-          await this.setCapabilityValue("onoff", value);
-        }
-        return;
-      }
-
-      // Geen print bezig, dus blokkeren
+    if (isPrinting) {
       if (value) {
-        throw new Error("Kan geen print hervatten als er geen print bezig is.");
+        await this.client.resumePrint();
       } else {
-        throw new Error("Kan geen print pauzeren als er geen print bezig is.");
+        await this.client.pausePrint();
       }
-    } finally {
-      // Stop altijd de keepalive, ongeacht succes of fout
-      await this.client.stopKeepAlive(true);
+      await this.setCapabilityValue("onoff", value);
+    } else {
+      throw new Error(value
+        ? "Kan geen print hervatten als er geen print bezig is."
+        : "Kan geen print pauzeren als er geen print bezig is.");
     }
   }
 
   async updateStatus() {
-    if (!this.client) {
-      return;
-    }
-    try {
-      // Initialize control connection first
-      await this.client.initControl();
+    if (!this.client) return;
 
-      const temp = await this.client.getTempInfo();
-      const printStatus = await this.client.getPrintStatus();
+    try { 
+      const status = await this.client.getStatus();
 
-      const value = printStatus?.getPrintPercent();
-      const bedTemp = temp?.getBedTemp()?.getCurrent() ?? 100; // 100 by default otherwise it might show that print is stopped.
+      if (status.isPrinting) {
+        this.log(`Currently printing: ${status.printPercent}%`);
+        this.setCapabilityValue("measure_print_percentage", status.printPercent);
+        this.setCapabilityValue("onoff", true);
 
-      if (typeof value === 'number' && !isNaN(value)) { 
-        this.log("Currently printing: " + value + "%");
-        this.setCapabilityValue("measure_print_percentage", value)
-        this.setCapabilityValue("onoff", true)
-
-
-        if(value == 100 && bedTemp < 40) { 
+        if (status.printPercent === 100 && status.bedTemp < 40) {
           this.log("Cooldown period over, printing is officially done.");
-          this.setCapabilityValue("measure_print_percentage", 0)
-          this.setCapabilityValue("onoff", false)
+          this.setCapabilityValue("measure_print_percentage", 0);
+          this.setCapabilityValue("onoff", false);
         }
-
-      }
-      else {
+      } else {
         this.log("No print job found, disable and set percentage to 0");
-        this.setCapabilityValue("measure_print_percentage", 0)
-        this.setCapabilityValue("onoff", false)
+        this.setCapabilityValue("measure_print_percentage", 0);
+        this.setCapabilityValue("onoff", false);
       }
 
-      if (temp) {
-        this.log("Set extruder temperature: " + temp?.getExtruderTemp()?.getCurrent());
-        this.setCapabilityValue('measure_temperature.extruder', temp?.getExtruderTemp()?.getCurrent());
-        this.log("Set bed temperature: " + temp?.getBedTemp()?.getCurrent());
-        this.setCapabilityValue('measure_temperature.bed', temp?.getBedTemp()?.getCurrent());
-      }
-
-
-      await this.client.stopKeepAlive(true)
+      this.setCapabilityValue("measure_temperature.extruder", status.extruderTemp);
+      this.setCapabilityValue("measure_temperature.bed", status.bedTempRaw);
     } catch (error) {
       this.log("Error found, turn off and reset ( could be powered off printer )");
-      this.setCapabilityValue("measure_print_percentage", 0)
-      this.setCapabilityValue('onoff', false);
+      this.setCapabilityValue("measure_print_percentage", 0);
+      this.setCapabilityValue("onoff", false);
     }
   }
 
   async onDeleted() {
     if (this.pollInterval) clearInterval(this.pollInterval);
   }
-
 
   async onUninit() {
     if (this.pollInterval) clearInterval(this.pollInterval);
