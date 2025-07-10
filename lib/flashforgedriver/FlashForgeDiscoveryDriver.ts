4955c16636e9211e0aa4579d01f0ec43c7cf2a5f
arnovr <arnovanrossum1@gmail.com>
2025-07-10 17:36:28 +0200

    Check connection with the checkcode

--- lib/flashforgedriver/FlashForgeDiscoveryDriver.ts
+++ lib/flashforgedriver/FlashForgeDiscoveryDriver.ts
@@ -1,11 +1,11 @@
 import Homey from 'homey';
 import { FlashForgePrinter, FlashForgePrinterDiscovery } from 'ff-5mp-api-ts';
 import PairSession from 'homey/lib/PairSession';
+import { FlashForgeClient } from './api/FlashForgeClient';
 
 
 export class FlashForgeDiscoveryDriver extends Homey.Driver {
-
-  private devicesToPair: any[] = [];
+  private devicesToPair: FlashForgePrinter[] = [];
 
     async onInit() {
       this.log('FlashForgeDiscoveryDriver has been initialized');
@@ -17,26 +17,32 @@ export class FlashForgeDiscoveryDriver extends Homey.Driver {
       
       this.devicesToPair = await flashForgeDiscovery.discoverPrintersAsync() as FlashForgePrinter[]
       
-      return this.devicesToPair.map(p => {
-        return {
-          name: p.name,
-          data: {
-            serialNumber: p.serialNumber,
-          },
-          settings: {
-            ipAddress: p.ipAddress,
-            checkCode: ""
-          }
-        };
-      });
+      return this.devicesToPair;
     }
 
     async onPair(session: PairSession): Promise<void> {
       session.setHandler('list_devices', this.listDevices.bind(this));
 
       session.setHandler('saveDevice', async ({ checkCode }) => {
-        const printer = this.devicesToPair.pop(); // should always be one, singular = true on driver.compose.json
+        if(this.devicesToPair.length == 0 ) {
+          throw new Error('Printer not found.');
+        }
+        const printer = this.devicesToPair[0]; // should always be one, singular = true on driver.compose.json
         if (!printer) throw new Error('Printer not found.');
+
+        const client = new FlashForgeClient({
+          ipAddress: printer.ipAddress,
+          serialNumber: printer.serialNumber,
+          checkCode: checkCode
+        });
+
+        const connected = await client.connect();
+        if(connected) {
+          await client.disconnect();
+        }
+        else {
+          throw new Error('Could not connect to printer, wrong checkcode?');
+        }
 
         // Pass full data to the frontend so it could create the device
         session.emit("createDevice", {
