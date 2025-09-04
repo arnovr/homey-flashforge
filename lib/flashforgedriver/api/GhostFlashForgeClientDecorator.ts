b72918c7da422f1f5add90a25802edb9c5c084f0
arnovr <arnovanrossum1@gmail.com>
2025-09-04 09:42:59 +0200

    Fix the rounding issue on the print percentage. And assume higher extruder temperature to be start printing

--- lib/flashforgedriver/api/GhostFlashForgeClientDecorator.ts
+++ lib/flashforgedriver/api/GhostFlashForgeClientDecorator.ts
@@ -17,22 +17,30 @@ export class GhostFlashForgeClientDecorator implements NormalizedPrinterClient {
       const extruderTemp = temp?.getExtruderTemp?.()?.getCurrent?.() ?? 0;
   
       return {
-        isPrinting: this.checkIsPrinting(printPercent),
+        isPrinting: this.checkIsPrinting(extruderTemp, printPercent),
         printPercent: printPercent,
         bedTemp,
         extruderTemp
       };
     }
 
-    private checkIsPrinting(printPercent: number): boolean {
-      return printPercent > 0 && printPercent < 100;
+    private checkIsPrinting(extruderTemp: number, printPercent: number): boolean {
+      if(printPercent == 0 ) {
+        return extruderTemp > 70;
+      }
+      return printPercent < 100;
     }
 
     private getPrintPercent(status: PrintStatus): number {
       // The format is "current/total"
       const [current, total] = status.getSdProgress().split("/");
-      if(isNaN(parseInt(current)) || isNaN(parseInt(total))) return 0;
-      return parseInt(current) / parseInt(total) * 100;
+
+      const c = parseInt(current, 10);
+      const t = parseInt(total, 10);
+
+      if (isNaN(c) || isNaN(t) || t === 0) return 0;
+
+      return Math.round((c / t) * 100);
     }
   
     async pause() {
@@ -44,11 +52,8 @@ export class GhostFlashForgeClientDecorator implements NormalizedPrinterClient {
     }
   
     async isPrinting(): Promise<boolean> {
-      const status = await this.client.getPrintStatus();
-      if(!status) return false;
-      
-      const percent = this.getPrintPercent(status);
-      return this.checkIsPrinting(percent);
+      const status = await this.getStatus();
+      return status.isPrinting;
     }
   
     async getPrinterName(): Promise<string> {
